f182ca6c73417f9d2d187e0565a82c5c75d73787
hasashin <dominik.hazak@gmail.com>
2021-01-22 17:20:20 +0100

    Added pointcloud2 compression

--- src/compressor.py
+++ src/compressor.py
@@ -1,38 +1,79 @@
 #!/usr/bin/env python
 import rospy
 from nav_msgs.msg import OccupancyGrid
-from map_compressor.msg import CompressedMap
+from sensor_msgs.msg import PointCloud2
+from map_compressor.msg import CompressedMap, CompressedPontCloud2
 import zlib
 import base64
-import time
+import sys
 
 current_map = OccupancyGrid()
+current_pointcloud = PointCloud2()
 compressed_map = CompressedMap()
+compressed_pointcloud = CompressedPontCloud2()
 
 def map_callback(the_map):
     global current_map 
     global compressed_map
     current_map = the_map
-    rospy.loginfo('Starting compression')
     compressed_map = CompressedMap()
     compressed_map.header = the_map.header
     compressed_map.info = the_map.info
+    rospy.loginfo('Compressing map for {}'.format(the_map.header.frame_id))
     new_data = []
     for i in the_map.data:
         new_data.append(i+127)
     compressed_data = zlib.compress(str(bytearray(new_data)))
     compressed_map.data = base64.b64encode(compressed_data)
 
+def pointcloud_callback(the_pointcloud):
+    global current_pointcloud
+    global compressed_pointcloud
+    current_pointcloud = the_pointcloud
+    compressed_pointcloud = CompressedPontCloud2()
+    compressed_pointcloud.header = the_pointcloud.header
+    compressed_pointcloud.height = the_pointcloud.height
+    compressed_pointcloud.width = the_pointcloud.width
+    compressed_pointcloud.is_bigendian = the_pointcloud.is_bigendian
+    compressed_pointcloud.point_step = the_pointcloud.point_step
+    compressed_pointcloud.row_step = the_pointcloud.row_step
+    compressed_pointcloud.is_dense - the_pointcloud.is_dense
+    rospy.loginfo('Compressing pointcloud for {}'.format(the_pointcloud.header.frame_id))
+    new_fields = []
+    for field in the_pointcloud.fields:
+        compressed_field = field.name
+        compressed_field_data  = bytearray()
+        compressed_field_data.append(field.offset)
+        compressed_field_data.append(field.datatype)
+        compressed_field_data.append(field.count)
+        temp = zlib.compress(str(compressed_field_data))
+        compressed_field = compressed_field + base64.b64encode(temp)
+        new_fields.append(compressed_field)
+    compressed_pointcloud.fields = new_fields
+    new_data = []
+    for entry in the_pointcloud.data:
+        new_data.append(entry)
+    compressed_data = zlib.compress(str(bytearray(new_data)))
+    compressed_pointcloud.data = base64.b64encode(compressed_data)
 
-sub = rospy.Subscriber('/map', OccupancyGrid, callback=map_callback)
-pub = rospy.Publisher('/compressed_map', CompressedMap, queue_size=2)
+if len(sys.argv)-1 >= 1:
+    poincloud_topic = sys.argv[1]
+else:
+    print 'You have to provide pointcloud topic'
+    exit(-1)
+
+sub_map = rospy.Subscriber('/map', OccupancyGrid, callback=map_callback)
+pub_map = rospy.Publisher('/compressed_map', CompressedMap, queue_size=2)
+sub_point = rospy.Subscriber('', PointCloud2, callback=pointcloud_callback)
+pub_point = rospy.Publisher('/compressed_pointcloud2', CompressedPontCloud2, queue_size=2)
 rospy.init_node('map_compressor')
 rate = rospy.Rate(1)
 
 rospy.loginfo("Map Compressor started")
 
 while not rospy.is_shutdown():
-    pub.publish(compressed_map)
+    pub_map.publish(compressed_map)
+    pub_map.publish(compressed_pointcloud)
     rate.sleep()
 
 rospy.loginfo("Map Compressor is stopping")
